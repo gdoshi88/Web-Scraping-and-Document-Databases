6ba98551bfb3b2751c1d645d60064bb5c746febe
Gautam Doshi <gdoshi88@gmail.com>
2019-08-19 18:35:39 -0700

    Updated_HTML_and_app

--- app.py
+++ app.py
@@ -1,25 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import scrape_mars
-import pymongo
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
-conn = 'mongodb://localhost:27017/mission_to_mars'
-client = pymongo.MongoClient(conn)
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/mission_to_mars'
+mongo = PyMongo(app)
 
 
 @app.route('/')
 def index():
-    mars = client.db.mars.find_one()
-    return render_template("index.html", mars='mars')
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars=mars)
 
 
 @app.route('/scrape')
 def scrape():
-    mars = client.db.mars
+    mars = mongo.db.mars
     mars_data = scrape_mars.scrape()
-    mars.update({}, mars_data)
-    return "Web Scrape Completed"
+    mars.update({}, mars_data, upsert=True)
+    return redirect('/', code=302)
 
 
 if __name__ == "__main__":
--- scrape_mars.py
+++ scrape_mars.py
@@ -16,7 +16,7 @@ def scrape():
         'title': nasa_mars_title,
         'paragraph': nasa_mars_paragraph,
         'featured_image': featured_img_url_two(browser),
-        'facts': df(),
+        'facts': dataframe(),
         'hemispheres': hemisphere_image_urls(browser)
     }
 
@@ -53,26 +53,24 @@ def featured_img_url_two(browser):
         "div", class_="carousel_items").find("article")["style"]
     featured_img_url = featured_img_url_raw.split("'")[1]
 
-    # In[260]:
-
     featured_img_base = "https://www.jpl.nasa.gov"
     featured_img_url_two = featured_img_base + featured_img_url
 
     return featured_img_url_two
 
 
-def df():
+def dataframe():
     url = "https://space-facts.com/mars/"
 
     tables = pd.read_html(url)
 
-    df = tables[0]
-    df[['Mars - Earth Comparison', 'Mars']]
-    df.set_index('Mars - Earth Comparison', inplace=True)
+    df = tables[1]
+    df.columns = ['Property', 'Number']
+    df.set_index('Property', inplace=True)
     df.to_html()
     df.replace('\n', '')
 
-    return df
+    return df.to_dict()
 
 
 def hemisphere_image_urls(browser):
@@ -80,20 +78,18 @@ def hemisphere_image_urls(browser):
     browser.visit(url)
     html = browser.html
     hemi_soup = bs(html, 'html.parser')
-    hemi_strings = []
     links = hemi_soup.find_all('h3')
+    hemisphere_image_urls = []
+    hemi_list = []
 
     for hemi in links:
-        hemi_strings.append(hemi.text)
+        hemi_list.append(hemi.text)
 
-    hemisphere_image_urls = []
-
-    for hemi in hemi_strings:
+    for hemi in hemi_list:
         hemi_dict = {}
         browser.click_link_by_partial_text(hemi)
         hemi_dict["img_url"] = browser.find_by_text('Sample')['href']
         hemi_dict["title"] = hemi
         hemisphere_image_urls.append(hemi_dict)
         browser.back()
-
     return hemisphere_image_urls
